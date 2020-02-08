2e038ecd1d92738770d9ae0e4364b9a138d8ee68
seandurban <seandurban10@gmail.com>
2020-02-08 23:32:56 +0000

    Add hashing functionality

--- Utils/urlhasher.js
+++ Utils/urlhasher.js
@@ -1,5 +1,9 @@
+var crypto = require('crypto');
+
 module.exports = {
   getUrlHash : function (originalUrl) {
-    return 'fakehash';
+    // Date.now() concat to ensure that identical urls get unique hash
+    var uniqueUrl = originalUrl + Date.now();
+    return crypto.createHash('md5').update(uniqueUrl).digest("base64").slice(0,6);
   }
 }
