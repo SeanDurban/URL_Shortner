c4273ce99d51ae83e32fb814666275565c5a3812
seandurban <seandurban10@gmail.com>
2020-02-08 00:07:41 +0000

    Upded URL Router for GET and POST - params/body working

--- routes/url.js
+++ routes/url.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var urlHasher = require('.././Utils/urlhasher');
+var dbhandler = require('.././Utils/dbhandler');
+
+
+router.get('/:shortUrl', (req, res) => {
+  var dbResult = dbhandler.getOriginalUrl(req.params.shortUrl);
+  res.send(dbResult);
+});
 
 /* GET a long URL for provided short URL. */
 router.get('/', function(req, res, next) {
+  res.send("Must specify the shortened url");
+});
 
-  // req.body.shorturl.split('http://tier.app/')[0]
-  // db lookup for the shorturl
-  let longUrl = '/';
-  res.redirect(longUrl);
+
+
+router.post('/', (req, res) => {
+  console.log(req.body.longUrl);
+
+  let hash = urlHasher.getUrlHash(req.body.longUrl);
+
+  res.send(hash + '\nbody \n' + JSON.stringify(req.body));
 });
 
 module.exports = router;
