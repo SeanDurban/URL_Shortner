4817a3895099671b2d24c4c201cd7d4b1934313f
seandurban <seandurban10@gmail.com>
2020-02-09 00:05:59 +0000

    Add Stat model

---
+++ models/Stat.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+// Stat contains mapping between hashedUrl and the number of visits (lookups)
+// Stat Schema definition
+var statSchema = mongoose.Schema({
+  // Not necessary to include whole shortened url
+  hashedUrl : {
+    type: String,
+    required: [true, 'Hashed Url required']
+  },
+  numberVisits : {
+    type: Integer,
+    default: 0
+  }
+});
+
+// Compile the model with statSchema
+var Stat = mongoose.model('Stat', statSchema );
