62c2db56fe58b0d5fafd74a995d4a05ea9cde9ee
seandurban <seandurban10@gmail.com>
2020-02-10 02:13:31 +0000

    Add barebones to the index router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET homepage for url shortner. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'URL Shortner' });
+  let shortenedUrl = req.body.shortenedUrl ? req.body.shortenedUrl : "" ;
+  res.render('index', {shortenedUrl: shortenedUrl});
+});
+
+
+router.post('/', (req, res) => {
+  let originalUrl = req.body.originalUrl;
+
+  //Some validation on the originalUrl should go here
+  if(originalUrl == null) {
+    res.status(400).send("Must specify originalUrl as POST body property").end();
+  }
+  else {
+      //use own api for post request
+
+  }
 });
 
 module.exports = router;
